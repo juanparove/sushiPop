4deb35672d7398d513f5d92ccac3530e4b851707
fmelo307 <fernando.melo@outlook.com.ar>
2023-08-08 10:10:31 -0300

    INIT

---
+++ SushiPOP-YA1A-2C2023-G3/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SushiPOP_YA1A_2C2023_G3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
