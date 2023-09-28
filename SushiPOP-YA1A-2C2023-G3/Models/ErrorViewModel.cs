434b9987e208cd35bfc6e7cd3e4b3c9b2764ff4e
Mauricio <117361386+Mauro0800@users.noreply.github.com>
2023-09-27 21:59:52 -0300

    Agrega mensajes de error en ErrorViewModel

--- SushiPOP-YA1A-2C2023-G3/Models/ErrorViewModel.cs
+++ SushiPOP-YA1A-2C2023-G3/Models/ErrorViewModel.cs
@@ -5,5 +5,10 @@ namespace SushiPOP_YA1A_2C2023_G3.Models
         public string? RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+        public const string CampoRequerido = "{0} es un campo obligatorio.";
+        public const string CaracteresMinimos = "{0} debe tener al menos {1} caracteres.";
+        public const string CaracteresMaximos = "{0} debe tener menos de {1} caracteres.";
+        public const string ValorMinMax = "{0} debe tener un valor entre {1} y {2}.";
     }
 }
